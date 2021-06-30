7a72c4127aa510f6c91654f746522e17d8903abb
miarunb <miarunb@yandex.ru>
2021-06-30 14:55:38 +0600

    Created models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    image = models.ImageField(upload_to='brands', blank=True)
+    slug = models.SlugField(max_length=200, db_index=True, primary_key=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    brand = models.ForeignKey(Brand, related_name='brand', blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, primary_key=True)
+    image = models.ImageField(upload_to='products', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
